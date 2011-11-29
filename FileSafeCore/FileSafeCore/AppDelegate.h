84466cfbc04abe34040d229aef657261bc5f2acd
Moses Mendoza <mendoza.moses@gmail.com>
2011-11-29 12:46:21 -0800

    Added |source...| button, IBAction to select directories/files for summing

--- FileSafeCore/FileSafeCore/AppDelegate.h
+++ FileSafeCore/FileSafeCore/AppDelegate.h
@@ -3,7 +3,12 @@
 //  FileSafeCore
 //
 //  Created by mo on 11/29/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Atlas Developers LLC. All rights reserved.
+//
+// AppDelegate for FileSafeCore
+// FileSafeCore contains the core functionality of the FileSafeManager
+// The AppDelegate is a proving ground/test bed for the various functions
+// that will eventually make it into the FileSafeManager
 //
 
 #import <Cocoa/Cocoa.h>
@@ -11,5 +16,6 @@
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+- (IBAction)chooseSource:(id)sender;
 
 @end
--- FileSafeCore/FileSafeCore/AppDelegate.m
+++ FileSafeCore/FileSafeCore/AppDelegate.m
@@ -2,19 +2,36 @@
 //  AppDelegate.m
 //  FileSafeCore
 //
-//  Created by mo on 11/29/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Created by Moses Mendoza 11/29/11.
+//  Copyright (c) 2011 Atlas Developers LLC. All rights reserved.
 //
+//  
 
 #import "AppDelegate.h"
 
 @implementation AppDelegate
-
 @synthesize window = _window;
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-    // Insert code here to initialize your application
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification{
 }
 
+//  chooseSource is the action that responds to the |Source...| button of the
+//  the main window inteface. The NSOpenPanel allows user to select a 
+//  directory path(s) or file(s) to be summed
+- (IBAction)chooseSource:(id)sender {
+
+    NSOpenPanel *sourceSelectPanel = [NSOpenPanel openPanel];
+    [sourceSelectPanel setCanChooseDirectories:TRUE];
+    [sourceSelectPanel setAllowsMultipleSelection:TRUE];
+    
+    void(^handler)(NSInteger) = ^(NSInteger result){
+        if(result == NSFileHandlingPanelOKButton){
+            NSArray *filePaths = [sourceSelectPanel URLs];
+            NSLog(@"Paths selected: %@",filePaths);
+        }
+    };
+
+    [sourceSelectPanel beginSheetModalForWindow:_window completionHandler:handler];
+
+}
 @end
