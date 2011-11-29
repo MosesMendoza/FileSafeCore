0b97410c723795edf30d8ac55b87aef24835fb1c
Moses Mendoza <mendoza.moses@gmail.com>
2011-11-29 11:44:57 -0800

    Initial commit

---
+++ FileSafeCore/FileSafeCore/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  FileSafeCore
+//
+//  Created by mo on 11/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
